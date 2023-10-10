ba6ee88f0821bfccccf7508c0f3ac68265b46200
desgh2 <wdesgit@gmail.com>
2023-10-10 16:42:19 +0300

    init commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        loop: true,
+        dots: false,
+        nav: true,
+        navText: ["<i class='bi bi-chevron-left'></i>", "<i class='bi bi-chevron-right'></i>"]
+    });
+
+});
