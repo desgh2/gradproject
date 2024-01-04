b430699fa412deeff89b942b25d978cfc0359b3c
desgh2 <wdesgit@gmail.com>
2024-01-04 17:03:27 +0200

    all files

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 jQuery(document).ready(function($) {
 
     $('.owl-carousel').owlCarousel({
+        responsive: {
+            0: {
+                items: 1,
+            },
+            576: {
+                items: 1,
+            },
+            768: {
+                items: 1,
+            },
+            992: {
+                items: 3,
+            }
+        },
         center: true,
         loop: true,
         dots: false,
@@ -8,4 +22,18 @@ jQuery(document).ready(function($) {
         navText: ["<i class='bi bi-chevron-left'></i>", "<i class='bi bi-chevron-right'></i>"]
     });
 
+    $("#scrolltop").click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        });
+        return false;
+    });
+
+    $(window).bind('scroll', function() {
+        if ($(this).scrollTop() > 200) {
+            $("#scrolltop").show();
+        } else {
+            $("#scrolltop").hide();
+        }
+    });
 });
